6532fa53715714e2b2361d21ebfcefd21d102b9d
raqssr <raquel.oliveira.ramos@ua.pt>
2018-03-04 21:43:25 +0000

    pets cards done but with static info

--- app/src/main/java/com/example/ritasantiago/vetcare/Pets.java
+++ app/src/main/java/com/example/ritasantiago/vetcare/Pets.java
@@ -1,11 +1,17 @@
 package com.example.ritasantiago.vetcare;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by raquelramos on 04-03-2018.
@@ -13,14 +19,40 @@ import android.view.ViewGroup;
 
 public class Pets extends Fragment {
 
-    @Nullable
-    @Override
-    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        //returning our layout file
-        //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_pets, container, false);
+    private List<Person> persons;
+
+    class Person {
+        String name;
+        String age;
+        int photoId;
+
+        Person(String name, String age, int photoId) {
+            this.name = name;
+            this.age = age;
+            this.photoId = photoId;
+        }
     }
 
+    private void initializeData(){
+        persons = new ArrayList<>();
+        persons.add(new Person("Emma Wilson", "23 years old", R.drawable.emma));
+        persons.add(new Person("Lavery Maiss", "25 years old", R.drawable.lavery));
+        persons.add(new Person("Lillie Watts", "35 years old", R.drawable.lillie));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, @Nullable Bundle savedInstanceState) {
+        //returning our layout file
+        //change R.layout.yourlayoutfilename for each of your fragments
+        View rootView = inflater.inflate(R.layout.fragment_pets, container, false);
+        RecyclerView rv = (RecyclerView) rootView.findViewById(R.id.rv);
+        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
+        rv.setLayoutManager(llm);
+        initializeData();
+        RVPetAdapter adapter = new RVPetAdapter(persons);
+        rv.setAdapter(adapter);
+        return rootView;
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
---
+++ app/src/main/java/com/example/ritasantiago/vetcare/RVPetAdapter.java
@@ -0,0 +1,61 @@
+package com.example.ritasantiago.vetcare;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import java.util.List;
+
+/**
+ * Created by raquelramos on 04-03-2018.
+ */
+
+public class RVPetAdapter extends RecyclerView.Adapter<RVPetAdapter.PersonViewHolder>
+{
+    List<Pets.Person> persons;
+    RVPetAdapter(List<Pets.Person> persons){
+        this.persons = persons;
+    }
+
+    @Override
+    public PersonViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.cardview_pets, parent, false);
+        PersonViewHolder pvh = new PersonViewHolder(v);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(PersonViewHolder holder, int position) {
+        holder.personName.setText(persons.get(position).name);
+        holder.personAge.setText(persons.get(position).age);
+        holder.personPhoto.setImageResource(persons.get(position).photoId);
+    }
+
+    @Override
+    public int getItemCount() {
+        return persons.size();
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    public static class PersonViewHolder extends RecyclerView.ViewHolder {
+        CardView cv;
+        TextView personName;
+        TextView personAge;
+        ImageView personPhoto;
+
+        PersonViewHolder(View itemView) {
+            super(itemView);
+            cv = (CardView)itemView.findViewById(R.id.cv);
+            personName = (TextView)itemView.findViewById(R.id.person_name);
+            personAge = (TextView)itemView.findViewById(R.id.person_age);
+            personPhoto = (ImageView)itemView.findViewById(R.id.person_photo);
+        }
+    }
+}
