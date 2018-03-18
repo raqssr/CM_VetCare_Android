e2482c78e00e0dc48374c13f06e278c805bc5cab
raqssr <raquel.oliveira.ramos@ua.pt>
2018-03-18 14:35:34 +0000

    all interfaces from profile done. missing connection to db in these interfaces

--- app/src/main/java/com/example/ritasantiago/vetcare/ProceduresTabFragment.java
+++ app/src/main/java/com/example/ritasantiago/vetcare/ProceduresTabFragment.java
@@ -3,9 +3,15 @@ package com.example.ritasantiago.vetcare;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by raquelramos on 04-03-2018.
@@ -13,12 +19,29 @@ import android.view.ViewGroup;
 
 public class ProceduresTabFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         //returning our layout file
         //change R.layout.yourlayoutfilename for each of your fragments
-        return inflater.inflate(R.layout.fragment_procedures_tab, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_procedures_tab, container, false);
+        recyclerView = (RecyclerView) rootView.findViewById(R.id.rv_procedures);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.addItemDecoration(new DividerItemDecoration(getContext(), DividerItemDecoration.VERTICAL));
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        List<String> input = new ArrayList<>();
+        for (int i = 0; i < 100; i++) {
+            input.add("Test" + i);
+        }// define an adapter
+        mAdapter = new ProcedureAdapter(input);
+        recyclerView.setAdapter(mAdapter);
+        return rootView;
     }
 
 
