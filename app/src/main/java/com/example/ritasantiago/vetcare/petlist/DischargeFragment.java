8dccb74530e39bee208e56fcb976136ad0f3da1a
raqssr <raquel.oliveira.ramos@ua.pt>
2018-03-22 18:14:30 +0000

    pdf being generated when there is discharge of animal

--- app/src/main/java/com/example/ritasantiago/vetcare/petlist/DischargeFragment.java
+++ app/src/main/java/com/example/ritasantiago/vetcare/petlist/DischargeFragment.java
@@ -1,12 +1,18 @@
 package com.example.ritasantiago.vetcare.petlist;
 
-import android.graphics.Rect;
-import android.graphics.pdf.PdfDocument;
-import android.graphics.pdf.PdfDocument.*;
-import android.os.AsyncTask;
+import android.Manifest;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.annotation.Nullable;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -18,13 +24,16 @@ import android.widget.Toast;
 import com.example.ritasantiago.vetcare.R;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
 
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.OutputStream;
-import java.text.SimpleDateFormat;
-import java.util.Calendar;
+import java.util.List;
 
 /**
  * Created by raquelramos on 21-03-2018.
@@ -32,8 +41,11 @@ import java.util.Calendar;
 
 public class DischargeFragment extends Fragment {
 
-    private EditText pdfContentView;
-    private Button btn;
+    private static final String TAG = "PdfCreatorActivity";
+    private EditText mContentEditText;
+    private Button mCreateButton;
+    private File pdfFile;
+    final private int REQUEST_CODE_ASK_PERMISSIONS = 111;
     private FirebaseFirestore db;
 
     private void deleteAnimal(String name){
@@ -59,14 +71,26 @@ public class DischargeFragment extends Fragment {
         //change R.layout.yourlayoutfilename for each of your fragments
         View rootView = inflater.inflate(R.layout.fragment_discharge, container, false);
 
-        pdfContentView = (EditText) rootView.findViewById(R.id.tv_teste);
-        btn = (Button) rootView.findViewById(R.id.gera);
-        btn.setOnClickListener(new View.OnClickListener() {
+        mContentEditText = (EditText) rootView.findViewById(R.id.tv_teste);
+        mCreateButton = (Button) rootView.findViewById(R.id.gera);
+        mCreateButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                new PdfGenerationTask().execute();
+            public void onClick(View v) {
+                if (mContentEditText.getText().toString().isEmpty()){
+                    mContentEditText.setError("Body is empty");
+                    mContentEditText.requestFocus();
+                    return;
+                }
+                try {
+                    createPdfWrapper();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (DocumentException e) {
+                    e.printStackTrace();
+                }
             }
         });
+
 
         return rootView;
     }
@@ -79,64 +103,101 @@ public class DischargeFragment extends Fragment {
         getActivity().setTitle("Discharge");
     }
 
-    /**
-     * Background task to generate pdf from users content
-     *
-     * @author androidsrc.net
-     */
-    private class PdfGenerationTask extends AsyncTask<Void, Void, String> {
+    private void createPdfWrapper() throws FileNotFoundException,DocumentException{
 
-        @Override
-        protected String doInBackground(Void... params) {
+        int hasWriteStoragePermission = ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (hasWriteStoragePermission != PackageManager.PERMISSION_GRANTED) {
 
-            PdfDocument document = new PdfDocument();
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!shouldShowRequestPermissionRationale(Manifest.permission.WRITE_CONTACTS)) {
+                    showMessageOKCancel("You need to allow access to Storage",
+                            new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                                        requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                                                REQUEST_CODE_ASK_PERMISSIONS);
+                                    }
+                                }
+                            });
+                    return;
+                }
 
-            // repaint the user's text into the page
-            View content = getActivity().findViewById(R.id.tv_teste);
-
-            // crate a page description
-            int pageNumber = 1;
-            PageInfo pageInfo = new PageInfo.Builder(content.getWidth(),
-                    content.getHeight() - 20, pageNumber).create();
-
-            // create a new page from the PageInfo
-            Page page = document.startPage(pageInfo);
-
-            content.draw(page.getCanvas());
-
-            // do final processing of the page
-            document.finishPage(page);
-
-            SimpleDateFormat sdf = new SimpleDateFormat("ddMMyyyyhhmmss");
-            String pdfName = "pdfdemo"
-                    + sdf.format(Calendar.getInstance().getTime()) + ".pdf";
-
-            File outputFile = new File("/storage/emulated/0/Download", pdfName);
-
-            try {
-                outputFile.createNewFile();
-                OutputStream out = new FileOutputStream(outputFile);
-                document.writeTo(out);
-                document.close();
-                out.close();
-            } catch (IOException e) {
-                e.printStackTrace();
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                        REQUEST_CODE_ASK_PERMISSIONS);
             }
+            return;
+        }else {
+            createPdf();
+        }
+    }
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_CODE_ASK_PERMISSIONS:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // Permission Granted
+                    try {
+                        createPdfWrapper();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (DocumentException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    // Permission Denied
+                    Toast.makeText(getActivity().getApplicationContext(), "WRITE_EXTERNAL Permission Denied", Toast.LENGTH_SHORT)
+                            .show();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+    private void showMessageOKCancel(String message, DialogInterface.OnClickListener okListener) {
+        new AlertDialog.Builder(getActivity())
+                .setMessage(message)
+                .setPositiveButton("OK", okListener)
+                .setNegativeButton("Cancel", null)
+                .create()
+                .show();
+    }
 
-            return outputFile.getPath();
+    private void createPdf() throws FileNotFoundException, DocumentException {
+
+        File docsFolder = new File(Environment.getExternalStorageDirectory() + "/Documents");
+        if (!docsFolder.exists()) {
+            docsFolder.mkdir();
+            Log.i(TAG, "Created a new directory for PDF");
         }
 
-        @Override
-        protected void onPostExecute(String filePath) {
-            if (filePath != null) {
-                btn.setEnabled(true);
-                pdfContentView.setText("");
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Pdf saved at " + filePath, Toast.LENGTH_SHORT).show();
-            } else {
-                Toast.makeText(getActivity().getApplicationContext(),
-                        "Error in Pdf creation" + filePath, Toast.LENGTH_SHORT).show();
-            }
+        pdfFile = new File(docsFolder.getAbsolutePath(),"HelloWorld.pdf");
+        OutputStream output = new FileOutputStream(pdfFile);
+        Document document = new Document();
+        PdfWriter.getInstance(document, output);
+        document.open();
+        document.add(new Paragraph(mContentEditText.getText().toString()));
+
+        document.close();
+        previewPdf();
+
+    }
+
+    private void previewPdf() {
+
+        PackageManager packageManager = getActivity().getPackageManager();
+        Intent testIntent = new Intent(Intent.ACTION_VIEW);
+        testIntent.setType("/storage/emulated/0/Download");
+        List list = packageManager.queryIntentActivities(testIntent, PackageManager.MATCH_DEFAULT_ONLY);
+        if (list.size() > 0) {
+            Intent intent = new Intent();
+            intent.setAction(Intent.ACTION_VIEW);
+            Uri uri = Uri.fromFile(pdfFile);
+            intent.setDataAndType(uri, "/storage/emulated/0/Download");
+
+            startActivity(intent);
+        }else{
+            Toast.makeText(getActivity().getApplicationContext(),"Download a PDF Viewer to see the generated PDF",Toast.LENGTH_SHORT).show();
         }
     }
 
